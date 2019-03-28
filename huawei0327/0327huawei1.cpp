4d57d41f07a4325cecb27fa2ae15a99fd339e408
LiYang07 <neowoolee@126.com>
2019-03-28 17:32:29 +0800

    “代码缩进”

--- huawei0327/0327huawei1.cpp
+++ huawei0327/0327huawei1.cpp
@@ -1,26 +1,28 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-int main(){
-int n;
-cin>>n;
-string code;
-cin>>code;
-vector<string> out;
-for(int i=0;i<n;i++){
-if(code[9*i]=='1')
-out.push_back(code.substr(9*i+1,8));
-else
+int main()
 {
-string substr=code.substr(9*i+1,8);
-reverse(substr.begin(),substr.end());
-out.push_back(substr);
-}
-}
-for(auto i:out)
+    int n;
+    cin >> n;
+    string code;
+    cin >> code;
+    vector<string> out;
+    for (int i = 0; i < n; i++)
+    {
+        if (code[9 * i] == '1')
+            out.push_back(code.substr(9 * i + 1, 8));
+        else
+        {
+            string substr = code.substr(9 * i + 1, 8);
+            reverse(substr.begin(), substr.end());
+            out.push_back(substr);
+        }
+    }
+    for (auto i : out)
 
-cout<<i<<" ";
+        cout << i << " ";
 }
